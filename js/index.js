a2f242fea3faf3b36edb81f8499231f36fbf0be5
bcjcodes <blessingakpanemmanuel@gmail.com>
2020-03-21 21:58:00 +0100

    task solution

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const myName = 'Blessing'
+const courses = ['Html', 'Css', 'Javascript', 'Nodejs']
+console.log(myName)
+console.log(courses)
+
+//Courses are in even numbers
+function displayEven (num) {
+  for (i = 1; i <= 200; i++) {
+    if (i % 2 === 0) {
+      console.log(i)
+    }
+  }
+}
+
+displayEven()
